1b8d5eac443e6ea8d00eb01d6f25ff1de442a20e
Fer <fjcesio@mail.austral.edu.ar>
2026-08-26 15:13:23 -0300

    implement: jwt guard

--- apps/api/src/modules/auth/auth.module.ts
+++ apps/api/src/modules/auth/auth.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { UserRepository } from '../user/user.repository';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+
+
 
 @Module({
+  imports: [
+    JwtModule.register({
+      global: true, // Hace que JwtService esté disponible en toda la app
+      secret: process.env.JWT_SECRET || 'super-secret-key',
+      signOptions: { expiresIn: '7d' },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService, UserRepository],
-  exports: [AuthService, UserRepository],
+  providers: [JwtAuthGuard, AuthService, UserRepository],
+  exports: [JwtAuthGuard, JwtModule, AuthService, UserRepository],
 })
 export class AuthModule {}
