133641f87e7ded5a4a642610ec58fd8a6c3442dc
Fer <fjcesio@mail.austral.edu.ar>
2026-08-26 10:37:37 -0300

    espero que esto ande

--- apps/api/src/modules/user/user.repository.ts
+++ apps/api/src/modules/user/user.repository.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { User } from '../../generated/prisma';
+import { User } from '../../generated/prisma/client';
 import { PrismaService } from '../../prisma/prisma.service';
 
 export interface CreateUserData {
